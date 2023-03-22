7d353f6736e2850c3e66eec8b07f9b559f9db74e
raamiiChu <87169493+raamiiChu@users.noreply.github.com>
2023-03-22 20:08:05 +0800

    Create Climbing-Stairs.py

---
+++ Dynamic-Programming/70-Climbing-Stairs/Climbing-Stairs.py
@@ -0,0 +1,14 @@
+# time: O(N)
+# space: O(N)
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n < 3:
+            return n
+
+        steps = [0,1,2]
+
+        for i in range(3, n+1):
+            steps.append(steps[i-1] + steps[i-2])
+
+        return steps[n]
